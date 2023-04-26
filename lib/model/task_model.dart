cabbc3b70094bcc80093c7d323bf1f73ed383496
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-04-26 11:09:12 +0530

    (Created) Task Model
    This model will be  important for CRUD application
    
    This is a class which certain properties
    
    Currenlt found no error

---
+++ lib/model/task_model.dart
@@ -0,0 +1,75 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class TaskModel {
+  final DateTime timeNeeded;
+  final String taskName;
+  final int priority;
+  bool isCompleted;
+  TaskModel({
+    required this.timeNeeded,
+    required this.taskName,
+    required this.priority,
+    this.isCompleted = false,
+  });
+
+  TaskModel copyWith({
+    DateTime? timeNeeded,
+    String? taskName,
+    int? priority,
+    bool? isCompleted,
+  }) {
+    return TaskModel(
+      timeNeeded: timeNeeded ?? this.timeNeeded,
+      taskName: taskName ?? this.taskName,
+      priority: priority ?? this.priority,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'timeNeeded': timeNeeded.millisecondsSinceEpoch,
+      'taskName': taskName,
+      'priority': priority,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  factory TaskModel.fromMap(Map<String, dynamic> map) {
+    return TaskModel(
+      timeNeeded: DateTime.fromMillisecondsSinceEpoch(map['timeNeeded'] as int),
+      taskName: map['taskName'] as String,
+      priority: map['priority'] as int,
+      isCompleted: map['isCompleted'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory TaskModel.fromJson(String source) =>
+      TaskModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'TaskModel(timeNeeded: $timeNeeded, taskName: $taskName, priority: $priority, isCompleted: $isCompleted)';
+  }
+
+  @override
+  bool operator ==(covariant TaskModel other) {
+    if (identical(this, other)) return true;
+
+    return other.timeNeeded == timeNeeded &&
+        other.taskName == taskName &&
+        other.priority == priority &&
+        other.isCompleted == isCompleted;
+  }
+
+  @override
+  int get hashCode {
+    return timeNeeded.hashCode ^
+        taskName.hashCode ^
+        priority.hashCode ^
+        isCompleted.hashCode;
+  }
+}

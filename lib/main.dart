7e39bfa417ab9124cb478695f57aaa619c9c12f0
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-04-26 12:57:04 +0530

    (Refactor)
    Remove home_screen and new file out of it

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'firebase_options.dart';
+import 'view/screen/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,81 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key,
-  });
-
-  @override
-  MyHomePageState createState() => MyHomePageState();
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  List<String> taskList = [];
-
-  void _addTask(String task) {
-    setState(() {
-      taskList.add(task);
-    });
-  }
-
-  void _showAddTaskBottomSheet() {
-    showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return SizedBox(
-            height: 200.0,
-            child: Column(
-              children: [
-                TextField(
-                  decoration: const InputDecoration(
-                    hintText: 'Task Name',
-                  ),
-                  onSubmitted: (value) {
-                    _addTask(value);
-                    Navigator.pop(context);
-                  },
-                ),
-                const TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Date and Time',
-                  ),
-                ),
-                const TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Priority',
-                  ),
-                ),
-              ],
-            ),
-          );
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Task List'),
-      ),
-      body: ListView.builder(
-        itemCount: taskList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(taskList[index]),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _showAddTaskBottomSheet(),
-        tooltip: 'Add Task',
-        child: const Icon(Icons.add),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
